5e4205ee9b4bf7fbb2f56f73a35cdcfe527eb496
Ginkoe <ginkoe@protonmail.com>
2020-11-20 00:17:18 +0100

    Fixed mistake

--- src/lib/error/CannotDisconnectWithoutConnection.ts
+++ src/lib/error/CannotDisconnectWithoutConnection.ts
@@ -1,11 +1,9 @@
-import { CannotConnectAlreadyConnected } from './CannotConnectAlreadyConnected';
-
 export class CannotDisconnectWithoutConnection extends Error {
   name = 'CannotDisconnectWithoutConnection';
 
   constructor(connectionName: string) {
     super();
-    Object.setPrototypeOf(this, CannotConnectAlreadyConnected.prototype);
+    Object.setPrototypeOf(this, CannotDisconnectWithoutConnection.prototype);
     this.message = `Cannot disconnect the connection "${connectionName}" without it being connected`;
   }
 }
