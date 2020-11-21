ca8b529d651a20d2fbee22a5d77e7df444cf722e
Ginkoe <ginkoe@protonmail.com>
2020-11-21 03:23:51 +0100

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export * from './lib/query/clauses/Ret';
 export * from './lib/query/clauses/Create';
 export * from './lib/query/clauses/Del';
 export * from './lib/query/clauses/Where';
+export * from './lib/query/clauses/withRelation';
